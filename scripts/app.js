c5b75422423075d300561aeb5767cb90ed455046
Amardeep Mann <amar2slick@gmail.com>
2023-01-19 03:34:20 -0800

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,225 @@
+let oneBtn = document.getElementById('oneBtn');
+let twoBtn = document.getElementById('twoBtn');
+let rulesBtn = document.getElementById('rulesBtn');
+let btnCont = document.getElementById('btnCont');
+let goBtnCont = document.getElementById('goBtnCont');
+let gameTxt = document.getElementById('gameTxt');
+let picksTxt = document.getElementById('picksTxt');
+let winTxt = document.getElementById('winTxt');
+let apiUrl = 'https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption';
+let cpuPick, userPick;
+let userScore = 0;
+let cpuScore = 0;
+let cpuWinners;
+
+// Function to simplify button creation
+function CreateBtn(btnID='', btnText='Primary', btnClass='btn-primary') {
+    let btn = document.createElement('button');
+    btn.id = btnID;
+    btn.type = 'button';
+    btn.className = 'btn ' + btnClass;
+    btn.textContent = btnText
+    return btn;
+}
+
+// Function simplify img creation
+function CreateImg(imgID='', imgScr='') {
+    let img = document.createElement('img');
+    img.id = imgID;
+    img.src = imgScr;
+    img.className = 'gameImg';
+    // img.width = 200;
+    return img;
+}
+
+// Function to trigger round options to show
+function ShowRounds() {
+    ClearGame();
+    gameTxt.textContent = 'How many rounds would you like to play?';
+    let oneRndBtn = CreateBtn('oneRndBtn', '1');
+    oneRndBtn.addEventListener('click', ShowPictures);
+    let fiveRndBtn = CreateBtn('fiveRndBtn', '5');
+    let sevenRndBtn = CreateBtn('sevenRndBtn', '7');
+    // console.log(oneRndBtn);
+    btnCont.append(oneRndBtn, fiveRndBtn, sevenRndBtn);
+}
+
+// Function to trigger game icons to show
+function ShowPictures() {
+
+    ClearGame();
+    gameTxt.textContent = 'Pick your poison';
+
+    let rockImg = CreateImg('rockImg', './assets/rock.png');
+    rockImg.addEventListener('click', PickRock);
+    let paperImg = CreateImg('paperImg', './assets/paper.png');
+    paperImg.addEventListener('click', PickPaper);
+    let scissorsImg = CreateImg('scissorsImg', './assets/scissors.png');
+    scissorsImg.addEventListener('click', PickScissors);
+    let lizardImg = CreateImg('lizardImg', './assets/lizard.png');
+    lizardImg.addEventListener('click', PickLizard);
+    let spockImg = CreateImg('spockImg', './assets/spock.png');
+    spockImg.addEventListener('click', PickSpock);
+
+    btnCont.append(rockImg, paperImg, scissorsImg, lizardImg, spockImg);
+
+    UpdateScores();
+}
+
+function UpdateScores() {
+    score1.innerText = 'Your Score: ' + userScore;
+    score2.innerText = 'CPU Score: ' + cpuScore;
+}
+
+async function PickRock() {
+    userPick = 'Rock'
+    console.log('\nUser picks: ' + userPick);
+    await CallApi(apiUrl);
+    console.log('3. cpuPick pick check: ' + cpuPick);
+    picksTxt.textContent = `You picked: Rock . . . . CPU picked: ${cpuPick}`;
+    if (cpuPick === 'Paper' || cpuPick === 'Spock') {
+        console.log('CPU wins!');
+        winTxt.textContent = "CPU wins!";
+        cpuScore++;
+    } else if (cpuPick === 'Rock') {
+        console.log('Draw!')
+        winTxt.textContent = "Draw!";
+    } else {
+        console.log('You win!');
+        winTxt.textContent = "You win!";
+        userScore++;
+    }
+    ClearRow();
+    PostRound();
+    UpdateScores();
+}
+
+async function PickPaper() {
+    userPick = 'Paper';
+    console.log('\nUser picks: ' + userPick);
+    await CallApi(apiUrl);
+    console.log('3. cpuPick pick check: ' + cpuPick);
+    picksTxt.textContent = `You picked: Paper . . . . CPU picked: ${cpuPick}`;
+    if (cpuPick === 'Scissors' || cpuPick === 'Lizard') {
+        console.log('CPU wins!');
+        winTxt.textContent = "CPU wins!";
+    } else if (cpuPick === 'Paper') {
+        console.log('Draw!');
+        winTxt.textContent = "Draw!";
+    } else {
+        console.log('You win!');
+        winTxt.textContent = "You win!";
+    }
+    ClearRow();
+    PostRound();
+}
+
+async function PickScissors() {
+    userPick = 'Scissors';
+    console.log('\nUser picks: ' + userPick);
+    await CallApi(apiUrl);
+    console.log('3. cpuPick pick check: ' + cpuPick);
+    picksTxt.textContent = `You picked: Scissors . . . . CPU picked: ${cpuPick}`;
+    if (cpuPick === 'Rock' || cpuPick === 'Spock') {
+        console.log('CPU wins!');
+        winTxt.textContent = "CPU wins!";
+    } else if (cpuPick === 'Scissors') {
+        console.log('Draw!');
+        winTxt.textContent = "Draw!";
+    } else {
+        console.log('You win!');
+        winTxt.textContent = "You win!";
+    }
+    ClearRow();
+    PostRound();
+}
+
+async function PickLizard() {
+    userPick = 'Lizard';
+    console.log('\nUser picks: ' + userPick);
+    await CallApi(apiUrl);
+    console.log('3. cpuPick pick check: ' + cpuPick);
+    picksTxt.textContent = `You picked: Lizard . . . . CPU picked: ${cpuPick}`;
+    if (cpuPick === 'Rock' || cpuPick === 'Scissors') {
+        console.log('CPU wins!');
+        winTxt.textContent = "CPU wins!";
+    } else if (cpuPick === 'Lizard') {
+        console.log('Draw!');
+        winTxt.textContent = "Draw!";
+    } else {
+        console.log('You win!');
+        winTxt.textContent = "You win!";
+    }
+    ClearRow();
+    PostRound();
+}
+
+async function PickSpock() {
+    userPick = 'Spock';
+    console.log('\nUser picks: ' + userPick);
+    await CallApi(apiUrl);
+    console.log('3. cpuPick pick check: ' + cpuPick);
+    picksTxt.textContent = `You picked: Spock . . . . CPU picked: ${cpuPick}`;
+    if (cpuPick === 'Paper' || cpuPick === 'Lizard') {
+        console.log('CPU wins!');
+        winTxt.textContent = "CPU wins!";
+    } else if (cpuPick === 'Spock') {
+        console.log('Draw!');
+        winTxt.textContent = "Draw!";
+    } else {
+        console.log('You win!');
+        winTxt.textContent = "You win!";
+    }
+    ClearRow();
+    PostRound();
+}
+
+function CheckWinner() {
+    
+}
+
+// Clears btnCont
+function ClearRow() {
+    btnCont.innerHTML = '';
+    goBtnCont.innerHTML = '';
+}
+
+function ClearGame() {
+    btnCont.innerHTML = '';
+    goBtnCont.innerHTML = '';
+    picksTxt.innerText = '';
+    winTxt.innerText = '';
+}
+
+// Sets cpuPick randomly
+async function CallApi(url){
+    await fetch(url).then(
+        response => response.text()
+    ).then(
+        data => {
+            console.log('1. data (in API call): ' + data);
+            cpuPick = data;
+            console.log('2. cpuPick (in API call): ' + cpuPick);
+        }
+    )
+}
+
+function PostRound() {
+    let p1Img = CreateImg(`${userPick.toLowerCase()}`, `./assets/${userPick.toLowerCase()}.png`);
+    let p2Img = CreateImg(`${cpuPick.toLowerCase()}`, `./assets/${cpuPick.toLowerCase()}.png`);
+
+    btnCont.append(p1Img, p2Img);
+    gameTxt.textContent = 'Game Over';
+
+    let playBtn = CreateBtn('playBtn', 'Play Again');
+    playBtn.addEventListener('click', ShowPictures);
+    let exitBtn = CreateBtn('exitBtn', 'Exit');
+
+    goBtnCont.append(playBtn, exitBtn);
+
+}
+
+// Called once to wake up API
+// CallApi(apiUrl);
+
+oneBtn.addEventListener('click', ShowRounds);
